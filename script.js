1a84f1be4181833e55176cc44b69ed57acc69ac0
Carlos Gadelha <josecarlos.gadelha10@gmail.com>
2022-02-11 18:27:46 -0300

    imprementação atualização das mensagens a cada 3 segundo e permanecer logado

--- script.js
+++ script.js
@@ -1,25 +1,65 @@
+let nomeUsuario
 
 
-let mensagens = document.querySelector("main .mensagens")
 
+nomeUsuario = prompt("Qual seu nome? ")
 
-const promessa = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
-promessa.then(processarResposta);
+const promise = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants',{
+   name: nomeUsuario
+})
 
-function processarResposta(resposta) {
-    console.log(resposta.data)
-    for (let i = 0; i < resposta.data.length; i++) {
-        
-        mensagens.innerHTML +=`
-            <div class="mensagem">
-                <p class="horario">${resposta.data[i].time} </p>
-                <p>${resposta.data[i].from} ${resposta.data[i].to}</p>
-                <p>${resposta.data[i].text} </p>
-            </div>
-            `
-        mensagens.lastElementChild.scrollIntoView()
+promise.then(conexaoSucesso)
+promise.catch(tratarErro);
 
+function conexaoSucesso(resposta) {
+    logado = true
+    const statusCode = parseInt(resposta.status)
+    if(statusCode === 200){
+        logado = true
+        console.log("Sucesso vai da certo Garoto")
+    }else if(statusCode === 400){
+        logado = false
     }
+    
+}
+
+function tratarErro(erro) {
+    console.log("Status code: " + erro.response.status); // Ex: 404
+    console.log("Mensagem de erro: " + erro.response.data); // Ex: Not Found
+}
+
+
+function PermanecerLogado(){
+    const promise = axios.post('https://mock-api.driven.com.br/api/v4/uol/status',{
+       name: nomeUsuario
+    })
+}
+
+function buscarMenssagens(){
+
+    let mensagens = document.querySelector("main .mensagens")
+
+    const promessa = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
+    promessa.then(processarResposta);
+
+    function processarResposta(resposta) {
+        // console.log(resposta.data)
+        for (let i = 0; i < resposta.data.length; i++) {
+            
+            mensagens.innerHTML +=`
+                <div class="mensagem ${resposta.data[i].type}" >
+                    <p>${resposta.data[i].time} </p>
+                    <p>${resposta.data[i].from} ${resposta.data[i].to}</p>
+                    <p>${resposta.data[i].text} </p>
+                </div>
+                `
+            mensagens.lastElementChild.scrollIntoView()
+
+        }
+}
 
 }
 
+
+setInterval(PermanecerLogado,5000)
+setInterval(buscarMenssagens,3000)
